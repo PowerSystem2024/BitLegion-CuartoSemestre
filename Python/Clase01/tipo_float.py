475cf8b63d26cef90ff1623c1d6db2c20c3eeecd
Daiana Cazón <cazondaie@gmail.com>
2025-10-11 22:01:10 -0300

    Clase 02: parte 3

---
+++ Python/Clase01/tipo_float.py
@@ -0,0 +1,13 @@
+# PROFUNDIZANDO EN EL TIPO FLOAT
+
+a = 3.0
+print(f'a: {a:.2f}')
+
+# Constructor de tipo float (recibe int && str)
+a = float(10) 
+a = float('10')
+print(f'a: {a:.2f}')
+
+# Notación exponencial (+-)
+a = 3e5
+print(f'a: {a}')
