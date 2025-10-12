f7040e4f3667166d79db235d844ed6241d64cec3
Daiana Cazón <cazondaie@gmail.com>
2025-10-11 22:05:41 -0300

    Clase 02: parte 4

--- Python/Clase01/tipo_float.py
+++ Python/Clase01/tipo_float.py
@@ -10,4 +10,14 @@ print(f'a: {a:.2f}')
 
 # Notación exponencial (+-)
 a = 3e5
-print(f'a: {a}')
+print(f'a: {a:.2f}')
+
+a = 3e-5
+print(f'a: {a:.5f}')
+
+
+# Cualquier cálculo que incluya un float cambiará a float
+a = 4.0 + 5
+print(a)
+print(type(a))
+
